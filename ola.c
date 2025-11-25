cbc382a1a5a2a6651365ff607be8f456f7e2a04a
ofabriciofarias <63527776+ofabriciofarias@users.noreply.github.com>
2025-11-25 09:59:37 -0300

    Código em sala

---
+++ ola.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Ola mundo! Estou vivo!");
+
+
+}
+
---
+++ olamundo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){ //ALGORITMO
+
+    int valor; //DECLARE valor NUMERICO
+
+    //Atribuição de valor
+    valor = 4; //valor <- 4
+
+    printf("Ola Mundo!\n"); //ESCREVA "Ola Mundo!"
+    printf("Estou no Brasil. V = %d\n", valor);
+
+} //FIM_ALGORITMO
