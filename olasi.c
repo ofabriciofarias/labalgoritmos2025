5e8ca66bc9c95efad62e77936b0d7de0018fe1d2
ofabriciofarias <63527776+ofabriciofarias@users.noreply.github.com>
2025-11-25 14:07:18 -0300

    Laboratório de algoritmos - Aula 01

---
+++ olasi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){ //ALGORITMO
+
+    printf("Ola SI!\n");
+    printf("Turma do terceiro turno!");
+
+} //FIM_ALGORITMO
---
+++ olaturma.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){ //ALGORITMO
+
+    printf("Ola Turma!\n"); //ESCREVA "Ola Turma!"
+    printf("A melhor turma de 2025\nNova Msg");
+} //FIM_ALGORITMO
+
